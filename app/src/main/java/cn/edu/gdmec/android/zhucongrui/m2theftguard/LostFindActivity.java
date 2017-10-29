4230324032f9bb7c4faef3f95e63f58732e97c9e
13124996357 <2501867629@qq.com>
2017-10-29 15:53:26 +0800

    3

--- app/src/main/java/cn/edu/gdmec/android/zhucongrui/m2theftguard/LostFindActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/zhucongrui/m2theftguard/LostFindActivity.java
@@ -1,27 +1,24 @@
 package cn.edu.gdmec.android.zhucongrui.m2theftguard;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import cn.edu.gdmec.android.zhucongrui.R;
 
 
-public class LostFindActivity extends AppCompatActivity /*implements View.OnClickListener*/{
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lost_find);
-        startSetup1Activity();
-    }
+public class LostFindActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private void startSetup1Activity() {
-        Intent intent = new Intent(LostFindActivity.this,SetUp1Activity.class);
-        startActivity(intent);
-        finish();
-    }
 
-    /*private TextView mSafePhoneTV;
+    private TextView mSafePhoneTV;
     private RelativeLayout mInterSetupRL;
     private SharedPreferences msharedPrefences;
     private ToggleButton mToggleButton;
@@ -96,5 +93,5 @@ public class LostFindActivity extends AppCompatActivity /*implements View.OnClic
                 break;
         }
 
-    }*/
+    }
 }
--- app/src/main/java/cn/edu/gdmec/android/zhucongrui/m2theftguard/SetUp4Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/zhucongrui/m2theftguard/SetUp4Activity.java
@@ -1,31 +1,18 @@
 package cn.edu.gdmec.android.zhucongrui.m2theftguard;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import cn.edu.gdmec.android.zhucongrui.R;
 
 
 public class SetUp4Activity extends BaseSetUpActivity{
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_set_up4);
-        ((RadioButton) findViewById(R.id.rb_four)).setChecked(true);
 
-    }
-
-    @Override
-    public void showNext() {
-        startActivityAndFinishShelf(LostFindActivity.class);
-    }
-
-    @Override
-
-    public void showPre() {
-        startActivityAndFinishShelf(SetUp3Activity.class);
-    }
-   /* private TextView mStatusTV;
+    private TextView mStatusTV;
     private ToggleButton mToggleButton;
 
     @Override
@@ -71,5 +58,5 @@ public class SetUp4Activity extends BaseSetUpActivity{
     @Override
     public void showPre(){
         startActivityAndFinishShelf(SetUp3Activity.class);
-    }*/
+    }
 }
