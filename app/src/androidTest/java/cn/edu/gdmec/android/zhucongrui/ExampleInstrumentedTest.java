7b49e9a8aac4503bcf2655e3ee2851f10ea6f25e
13124996357 <2501867629@qq.com>
2017-10-29 15:08:27 +0800

    3

--- app/src/androidTest/java/cn/edu/gdmec/android/zhucongrui/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cn/edu/gdmec/android/zhucongrui/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package cn.edu.gdmec.android.zhucongrui;
+package cn.edu.gdmec.android.zcr;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("cd.edu.gdmec.android.zcr", appContext.getPackageName());
+        assertEquals("", appContext.getPackageName());
     }
 }
