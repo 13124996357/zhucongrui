3e1b4c62b4d7422d2b3725984c1445ec3164e7bb
13124996357 <2501867629@qq.com>
2017-12-24 16:54:26 +0800

    最终

--- app/src/androidTest/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/db/dao/BlackNumberDaoTest.java
+++ app/src/androidTest/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/db/dao/BlackNumberDaoTest.java
@@ -2,23 +2,25 @@ package cn.edu.gdmec.android.mobileguard.m3communicationguard.db.dao;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import java.util.List;
 import java.util.Random;
 
 import cn.edu.gdmec.android.mobileguard.m3communicationguard.entity.BlackContactInfo;
 
-/**
- * Created by Administrator on 2017/11/4 0004.
- */
-@RunWith(AndroidJUnit4.class)
 
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class BlackNumberDaoTest {
     private Context context;
     private BlackNumberDao dao;
@@ -31,7 +33,7 @@ public class BlackNumberDaoTest {
 
     @Test
     public void t1Add() throws Exception {
-        Random random = new Random(8979);
+        Random random = new Random(8879);
         for (long i = 1; i < 30; i++) {
             BlackContactInfo info = new BlackContactInfo();
             info.phoneNumber = 13500000000l + i + "";
@@ -41,18 +43,22 @@ public class BlackNumberDaoTest {
         }
     }
 
+    /**
+     * 测试删除
+     */
+
     @Test
     public void t2Delete() throws Exception {
         BlackNumberDao dao = new BlackNumberDao(context);
         BlackContactInfo info = new BlackContactInfo();
         for (long i = 1; i < 5; i++) {
-
-            info.phoneNumber = 13500000000l + i + "";
-
+            info.phoneNumber = 13500000000l + 1 + "";
             dao.detele(info);
         }
+
     }
 
+    /*测试分页查询*/
     @Test
     public void t3GetPageBlackNumber() throws Exception {
         BlackNumberDao dao = new BlackNumberDao(context);
@@ -61,41 +67,32 @@ public class BlackNumberDaoTest {
             Log.i("TestBlackNumberDao", list.get(i).phoneNumber);
         }
     }
+/*测试根据号码查询黑名单信息*/
 
     @Test
-    public void t4GetBlackContactMode() throws Exception {
+    public void t4GetBlackContactMode() throws Exception{
         BlackNumberDao dao = new BlackNumberDao(context);
-        int mode = dao.getTotalNumber();
-        Log.i("TestBlackNumberDao", mode + "");
+        int mode = dao.getBlackContactMode(13500000000l+"");
+        Log.i("TestBlackNumberDao",mode+"");
 
     }
-
-    //"数据总条数:"+ total);
+    /**测试数据总条目*/
     @Test
-    public void t5GetTotalNumber() throws Exception {
+    public void t5GetTotalNumber() throws Exception{
         BlackNumberDao dao = new BlackNumberDao(context);
         int total = dao.getTotalNumber();
-        Log.i("TestBlackNumberDao", "数据总条目:" + total);
+        Log.i("TestBlackNumberDao","数据总条目: "+total);
     }
+/*测试号码是否在数据库中*/
 
     @Test
-    public void t6IsNumberExist() throws Exception {
+    public void t6IsNumberExist() throws Exception{
         BlackNumberDao dao = new BlackNumberDao(context);
-        boolean isExist = dao.IsNumberExist(13500000008l + "");
-        if (isExist) {
-            Log.i("TestBlackNumberDao", "该号码在数据库中");
-        } else {
-            Log.i("TestBlackNumberDao", "该号码不在数据库中");
+        boolean isExist = dao.IsNumberExist(13500000000l + "");
+        if (isExist){
+            Log.i("TestBlackNumberDao","该号码在数据库中");
+        }else{
+            Log.i("TestBlackNumberDao","该号码不在数据库中");
         }
-
     }
 }
-
-   /* @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("", appContext.getPackageName());
-    }
-}*/
